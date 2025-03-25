67b5e32287cc1d8726d75884703e923a64dd494d
6nce <vincentvtruong@gmail.com>
2025-03-25 11:28:35 -0700

    Reworked prefix-builder script to be run autonomously from the command line with arguments required.

--- scripts/prefix-builder.py
+++ scripts/prefix-builder.py
@@ -2,58 +2,44 @@ import os
 import sys
 import json
 import uuid
+import argparse
 
+def create_prefix(filepath, setname, prefix):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+    except Exception as e:
+        print(f"Error reading {filepath}: {e}")
+        return
+    
+    #Converts list to dict to make elements accessible by setname
+    newData = {x['name']: x for x in data['sets']}
+    category = newData[setname]['cards']
 
-#Looks for Argument/ Path to JSON. 
-if len(sys.argv) > 1:
-    workingJson = sys.argv[1]
-else:
-    #Drop JSON in Terminal or Copy/Paste Path file
-    workingJson=str(input("Insert Path to JSON Here: "))
-
-#Initial script setup
-f = open(workingJson)
-data = json.load(f)
-category = data['sets']
-
-#Assigns index to set and displays list
-x = 0
-for set in category:
-    print(str(x)+ " " + str(set['name']))
-    x+=1
-
-#User Prompts
-setChoice = int(input('Enter index of Set to edit '))
-selectOption = int(input('Select Option: \n1.Add Prefix \n2.Remove Prefix \n3.Convert Numbers to Signature Lettering \n'))
-prefix = input('Enter prefix for set: ')
-
-#Assigns index for set.
-category = data['sets'][setChoice]['cards']
-
-#Add Prefix
-if selectOption == 1:
+    #Add Prefix
     for card in category:
         card.update(number = prefix + "-" + card['number'])
-        print(card)
 
-#Remove Prefix 
-if selectOption == 2:
-    for card in category:
-        card.update(number = card['number'].replace(prefix + "-",""))
-        print(card)
+    #Convert back to JSON
+    json_object = json.dumps(data, indent=2)
 
-#Generates initials from Player Name
-def getInitials(name):
-    names = name.split(' ')
-    return f"{names[0][0]}{names[1][0]}"
+    with open(filepath,'w') as outfile:
+        outfile.write(json_object)
 
-#Convert Numbers to Signature Lettering, Adds Prefix
-if selectOption == 3:
-    for card in category:
-        card.update(number = prefix + "-" + getInitials(card['name']))
-        print(card)
+    #Success Message
+    print(prefix + " Added to " + setname)
 
-json_object = json.dumps(data, indent=2)
+def main(filepath, setname, prefix):
+    if not os.path.isfile(filepath):
+        print(f"Error: {filepath} is not a valid file.")
+        return
+    create_prefix(filepath, setname, prefix)
 
-with open(workingJson,'w') as outfile:
-    outfile.write(json_object)
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-j', '--json')
+    parser.add_argument('-s', '--set')
+    parser.add_argument('-p', '--prefix')
+    args=parser.parse_args()
+
+    main(args.json, args.set, args.prefix)
