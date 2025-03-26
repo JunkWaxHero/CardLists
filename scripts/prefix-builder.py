ca394d75863b3c84d6cde75bb9db64e109dee6d4
6nce <vincentvtruong@gmail.com>
2025-03-25 18:13:31 -0700

    Added versioning capabilities to prefix-builder.py

--- scripts/prefix-builder.py
+++ scripts/prefix-builder.py
@@ -1,8 +1,7 @@
 import os
-import sys
 import json
-import uuid
 import argparse
+from decimal import Decimal
 
 def create_prefix(filepath, setname, prefix):
     try:
@@ -14,20 +13,66 @@ def create_prefix(filepath, setname, prefix):
     
     #Converts list to dict to make elements accessible by setname
     newData = {x['name']: x for x in data['sets']}
-    category = newData[setname]['cards']
+    directory = newData[setname]['cards']
 
     #Add Prefix
-    for card in category:
+    for card in directory:
         card.update(number = prefix + "-" + card['number'])
+
+    #Update version number
+    major, minor = [int(x, 10) for x in data['version'].split('.')]
+    minor += 1
+    updatedVersion = str(major) + "." + str(minor)
+    data.update(version = updatedVersion)
+
+    splitName = data["name"].split(" ")
+    uniqueId = data["uniqueId"]
+    splitPath = filepath.split('/')
+
+    #Returns Year of Product
+    year = splitName[0]
+
+    #Returns Name of Product
+    productName = splitName[1]+" "+splitName[2]
+
+    #Returns Product category
+    sportCategory = splitPath[-3]
+
+    #Injects sportCategory ,retrieves category JSON, and loads into script
+    categoryPath = "../categories/"+sportCategory+".json"
+    c = open(categoryPath, 'r', encoding='utf-8')
+    category = json.load(c)
+
+    #Sorts Dict by Year then by Release
+    updatedCategory = {x['year']: x for x in category['category']['years']}
+    findYear = updatedCategory[year]['releases']
+    sortByName = {x['name']: x for x in findYear}
+
+    #Retrieves product information by name
+    getProduct = sortByName[productName]
+
+    #Checks uniqueId to confirm product identity before updating version.
+    if getProduct['uniqueId'] == uniqueId:
+        getProduct.update(version = updatedVersion)
 
     #Convert back to JSON
     json_object = json.dumps(data, indent=2)
+    json_object2 = json.dumps(category, indent=2)
 
+
+    #Write updates to JSON files
     with open(filepath,'w') as outfile:
         outfile.write(json_object)
 
+    with open(categoryPath,'w') as outfile:
+        outfile.write(json_object2)
+
+
     #Success Message
-    print(prefix + " Added to " + setname)
+    print(prefix + " Prefix Added to " + setname)
+    print("Version updated to: " + updatedVersion)
+    print(sportCategory+".json has been saved.")
+    print(splitPath[-1]+" has been saved.")
 
 def main(filepath, setname, prefix):
     if not os.path.isfile(filepath):
